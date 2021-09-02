10615892e66c14e2dba11b994472b0ec245dd871
Matthew Lee <ml16847@bristol.ac.uk>
2021-09-02 15:57:14 +0100

    upload gradient

---
+++ gradient/gradient1.R
@@ -0,0 +1,35 @@
+# environment ====
+rm(list=ls())
+set.seed(821)
+# packages ====
+library(tidyverse)
+library(poissoned)
+library(gganimate)
+## colours - https://mycolor.space/gradient
+
+# data ====
+pts <- poisson_disc(ncols = 100, nrows = 100, cell_size = 1, k = 100, 
+                    xinit = 40, yinit = 10, 
+                    keep_idx = TRUE, keep_boundary = F, verbose = T) %>% arrange(idx)
+
+# plot ====
+ggplot(pts) +
+  geom_point(aes(x = x, y = y, color = idx), size = 6, stroke = 0, alpha = 0.8) +
+  scale_color_gradientn(colors = c("#e2b396", "#edadb1", "#dfb0d4", "#babbeb", "#8dc7ec"), guide = "none") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#212124"))
+ggsave("gradient/gradient1.png", width = 1080, height = 1080, units = "px")
+
+# animate ====
+anim <-
+  ggplot(pts) +
+  geom_point(aes(x = x, y = y, color = idx, group = idx), size = 8, stroke = 0, alpha = 0.8) +
+  scale_color_gradientn(colors = c("#e2b396", "#edadb1", "#dfb0d4", "#babbeb", "#8dc7ec"), guide = "none") +
+  theme_void()  +
+  theme(panel.background = element_rect(fill = "#212124")) +
+  transition_reveal(along = idx) +
+  ease_aes("linear") +
+  enter_grow() +
+  enter_fade(alpha = 0.9) 
+animate(anim, nframes = 100, fps = 10, duration = 5, end_pause = 4)
+anim_save("gradient/gradient1.gif", animation = last_animation())
